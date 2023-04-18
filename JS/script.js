5a52e1965e14df1623c45bf3d6c0bcf66e4cfd88
Fabr1c100 <rm97631@fiap.com.br>
2023-04-18 20:19:33 -0300

    query e indexof

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,42 @@
-//Exemplo de funções
+// //Exemplo de funções
 
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
-// divElement.addEventListener("click", ()=> {
-//     console.log(divElement);
-// });
+// // divElement.addEventListener("click", ()=> {
+// //     console.log(divElement);
+// // });
 
-divElement.addEventListener("click", mostraElement);
+// divElement.addEventListener("click", mostraElement);
 
-function mostraElement(){
-    console.log(divElement)
-}
+// function mostraElement(){
+//     console.log(divElement)
+// }
 
-function soma(){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1+n2)
-}
+// function soma(){
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1+n2)
+// }
+
+// //Recuperando elementos do HTML com queryselector
+// const liElement = document.querySelector("")
+// console.log(liElement);
+
+ //Recuperando elementos do HTML com queryselector
+// const liElement = document.querySelectorAll("li");
+
+// //Convertendo NODE ist em array com spread
+// const arrayElement = [...liElement];
+
+// console.log(arrayElement);
+
+
+//Recuperando elementos do HTML com queryselector
+const arrayElements = [...document.querySelectorAll("li")];
+
+arrayElements.map((el,key)=>{
+
+    if (el.textContent.indexOf("1") != (-1)){
+        el.textContent = `[${el.textContent}]`;
+    }
+});
